73eb052c404b2143225e036264c6abf001a5dfa4
angelina <angelinak554@gmail.com>
2021-04-01 11:57:32 +0300

    initial commit1

--- main.py
+++ main.py
@@ -182,8 +182,8 @@ def main():
     # для одного объекта
     api.add_resource(news_resources.NewsResource, '/api/v2/news/<int:news_id>')
     port = int(os.environ.get("PORT", 5000))
-    #app.run(host='0.0.0.0', port=port)
-    app.run(host='127.0.0.1', port=port)
+    app.run(host='0.0.0.0', port=port)
+    #app.run(host='127.0.0.1', port=port)
     app.run()
 
 
